e9b0acb56be3f4afcbbe9474259c4157adb85b68
yoonpunk <yoonys427@outlook.com>
2023-02-12 12:21:43 +0900

    initial commit

---
+++ src/main/java/com/fnb/membership/fnbmembership/FnbMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.fnb.membership.fnbmembership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnbMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnbMembershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fnb/membership/fnbmembership/FnbMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnb.membership.fnbmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnbMembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
