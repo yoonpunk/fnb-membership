5a157682fab2c76126417b8381ea8b33c76eca78
yoonpunk <yoonys427@outlook.com>
2023-03-22 21:22:56 +0900

    Changed comments of EarnPointOrderByBarcode and EarnPointOrderByPhone

--- src/main/java/com/fnb/membership/fnbmembership/dto/EarnPointOrderByBarcode.java
+++ src/main/java/com/fnb/membership/fnbmembership/dto/EarnPointOrderByBarcode.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 
 /**
- * 휴대전화번호로 포인트 적립/사용 요청 시, 사용하는 Request/Response 객체
+ * 휴대전화번호로 포인트 적립 요청 시, 사용하는 Request/Response 객체
  * RequestPointOrderServiceController에서 사용
  */
 public class EarnPointOrderByBarcode {
